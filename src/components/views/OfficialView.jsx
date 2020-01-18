994fa493ef865a9e425a4ee4db8d177875d61109
Samson <samson50wu@gmail.com>
2020-01-18 10:57:42 -0500

    Installed disqus-react and embedded Disqus component into OfficialView

--- src/components/views/OfficialView.jsx
+++ src/components/views/OfficialView.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { OfficialCard } from "..";
+import Disqus from "disqus-react";
 
 // If you need cards or styling, you can uncomment the lines here to import
 // import "./OfficialView.css";
 
 const OfficialView = props => {
+  // Disqus configuration information
+  const disqusShortname = "voterbundle"; //found in your Disqus.com dashboard
+  const disqusConfig = {
+    url: "http://localhost:3000", //this.props.pageUrl
+    identifier: "article-id", //this.props.uniqueId
+    title: "Title of Your Article" //this.props.title
+  };
+
   return (
     <div>
       OfficialView here
       <OfficialCard />
+      <Disqus.DiscussionEmbed
+        shortname={disqusShortname}
+        config={disqusConfig}
+      />
     </div>
   );
 };
