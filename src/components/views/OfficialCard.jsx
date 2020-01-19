776b978bf785136f9a7bf31ea5abd5a04c9dd5b1
Samson <samson50wu@gmail.com>
2020-01-19 18:10:26 -0500

    Added some comments to OfficialCard

--- src/components/views/OfficialCard.jsx
+++ src/components/views/OfficialCard.jsx
@@ -26,31 +26,38 @@ const OfficialCard = props => {
 
       {/* Some officials don't have an address. */}
       {/* The address is an array holding a single object. */}
+      {/* Some addresses have a line2 and line3 etc in addition to just a line1 (Andrew M. Cuomo) So we'll have to iterate through them somehow.*/}
       {props.official.address ? (
         <div>
           <p>{props.official.address[0].line1}</p>
-          <p>{props.official.address[0].city}</p>
-          <p>{props.official.address[0].state}</p>
-          <p>{props.official.address[0].zip}</p>
+          <p>
+            {props.official.address[0].city}, {props.official.address[0].state}{" "}
+            {props.official.address[0].zip}
+          </p>
         </div>
       ) : (
         <div></div>
       )}
 
-      {/*  */}
+      {/* Officials will usually have one phone, url, and email */}
       {props.official.phones ? <p>{props.official.phones[0]}</p> : <div></div>}
-      {/*  */}
-      {props.official.urls ? <p>{props.official.urls[0]}</p> : <div></div>}
-      {/*  */}
-      {props.official.emails ? <p>{props.official.emails[0]}</p> : <div></div>}
-      {/*  */}
-      {props.official.channels ? (
-        <p>
-          {props.official.channels[0].type}: {props.official.channels[0].id}
-        </p>
+      {props.official.urls ? (
+        <a href={props.official.urls[0]}>{props.official.urls[0]}</a>
       ) : (
         <div></div>
       )}
+      {props.official.emails ? <p>{props.official.emails[0]}</p> : <div></div>}
+
+      {/* Officials can have anywhere from 0 to 3 channels! We will need to first see if they have any channels at all. If they do, we will then need to iterate through them to generate the elements. Ideally, we should be able to identify the domain of the channel, so that we can link to it directly. eg. https://www.facebook.com/newyorkstateag/ with the type is "Facebook" */}
+      {/* {props.official.channels ? (
+        <div>
+          <p>
+            {props.official.channels[0].type}: {props.official.channels[0].id}
+          </p>
+        </div>
+      ) : (
+        <div></div>
+      )} */}
     </div>
   );
 };
