e8a808890d682758d2af4ac12953c35a96eb3c2f
Isabella Berekdar <isabellaberekdar@gmail.com>
2020-01-19 00:11:32 -0500

    Added thunk to fetch images. Closes #18

--- src/components/containers/HomeContainer.jsx
+++ src/components/containers/HomeContainer.jsx
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from "react"
+import { connect } from "react-redux"
 // import { action } from "../../store/utilities/Home"; // Get the action creator for ____?
-import HomeView from "../views/HomeView";
-import { getPhotoThunk, getOfficialThunk } from '../../store/utilities/official'
+import HomeView from "../views/HomeView"
+import { getPhotoThunk, getOfficialThunk } from "../../store/utilities/official"
 
 class HomeContainer extends Component {
   constructor(props) {
-    super(props);
-    
+    super(props)
   }
-  
+
   componentDidMount() {
-    
-    this.props.getPhotoThunk('Charles', 'Schumer', 'NY')    
-    console.log(this.props.photo)
+    /*
+  this.props.getPhotoThunk('Charles', 'Schumer', 'NY')    
+    console.log(this.props.photo) */
   }
 
   render() {
@@ -22,21 +21,21 @@ class HomeContainer extends Component {
         <h1>HomeContainer here</h1>
         <HomeView />
       </div>
-    );
+    )
   }
 }
 
 const mapState = state => {
   return {
     photo: state.official
-  };
-};
+  }
+}
 
 const mapDispatch = dispatch => {
   return {
     getPhotoThunk: (first, last, state) => dispatch(getPhotoThunk(first, last, state)),
     getOfficialThunk: address => dispatch(getOfficialThunk(address))
-  };
-};
+  }
+}
 
-export default connect(mapState, mapDispatch)(HomeContainer);
+export default connect(mapState, mapDispatch)(HomeContainer)
