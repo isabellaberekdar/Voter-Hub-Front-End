f85ee7333012617cc51df7677cea3b6d51665f2f
Samson <samson50wu@gmail.com>
2020-01-19 17:13:07 -0500

    Added some style to officialCards

--- src/components/views/OfficialCard.jsx
+++ src/components/views/OfficialCard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-// import "../views/OfficialCard.css";
+import "../views/OfficialCard.css";
 
 const OfficialCard = props => {
   console.log(props);
   console.log(props.official.address);
   return (
-    <div>
+    <div className="official-card">
       <h3>OfficialCard here</h3>
       <p>{props.office.name}</p>
       <p>{props.office.divisionId}</p>
@@ -15,6 +15,14 @@ const OfficialCard = props => {
       {/* Some offices don't have levels or roles, so we need to check if they have them before trying to access them by index, or else it will result in an error. */}
       {props.office.levels ? <p>{props.office.levels[0]}</p> : <div></div>}
       {props.office.roles ? <p>{props.office.roles[0]}</p> : <div></div>}
+
+      <p>{props.official.name}</p>
+      <p>{props.official.party}</p>
+      {props.official.photoUrl ? (
+        <img src={props.official.photoUrl} width="200px" />
+      ) : (
+        <div></div>
+      )}
 
       {/* Some officials don't have an address. */}
       {/* The address is an array holding a single object. */}
@@ -30,8 +38,19 @@ const OfficialCard = props => {
       )}
 
       {/*  */}
+      {props.official.phones ? <p>{props.official.phones[0]}</p> : <div></div>}
       {/*  */}
+      {props.official.urls ? <p>{props.official.urls[0]}</p> : <div></div>}
       {/*  */}
+      {props.official.emails ? <p>{props.official.emails[0]}</p> : <div></div>}
+      {/*  */}
+      {props.official.channels ? (
+        <p>
+          {props.official.channels[0].type}: {props.official.channels[0].id}
+        </p>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
