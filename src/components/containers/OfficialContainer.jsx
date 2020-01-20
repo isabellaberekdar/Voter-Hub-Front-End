13d7fa67e9e77e2fdb621ad35522783c3ecd50b2
Samson <samson50wu@gmail.com>
2020-01-20 14:32:36 -0500

    Trying to fix the indexing through the URL

--- src/components/containers/OfficialContainer.jsx
+++ src/components/containers/OfficialContainer.jsx
@@ -1,10 +1,7 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import OfficialView from "../views/OfficialView";
-import {
-  getOfficialThunk,
-  getPhotoThunk
-} from "../../store/utilities/official";
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import OfficialView from "../views/OfficialView"
+import { getOfficialThunk, getPhotoThunk } from "../../store/utilities/official"
 
 class OfficialContainer extends Component {
   componentDidMount() {
@@ -17,14 +14,16 @@ class OfficialContainer extends Component {
     // this.props.getOfficial(state, index);
     // check if there is a photo. If not, go find one
 
-    const division = this.props.match.params.division;
-    const officeIndex = this.props.match.params.officeIndex;
-    const officialIndex = this.props.match.params.officialIndex;
-    console.log("kumquat", division, officeIndex, officialIndex);
+    const division = this.props.match.params.division
+    const officeIndex = this.props.match.params.officeIndex
+    const officialIndex = this.props.match.params.officialIndex
+    console.log("kumquat", division, officeIndex, officialIndex)
+
+    this.props.getOfficialThunk(division, officeIndex, officialIndex)
   }
 
   render() {
-    console.log(this.props.official);
+    console.log(this.props.official)
     return (
       <div>
         <h1>OfficialContainer here</h1>
@@ -35,7 +34,7 @@ class OfficialContainer extends Component {
           official="official here"
         />
       </div>
-    );
+    )
   }
 }
 
@@ -51,14 +50,15 @@ const mapState = state => {
     /*     official: "state.google.officials"
 
  */
-  };
-};
+  }
+}
 
 const mapDispatch = dispatch => {
   return {
-    getOfficial: (state, index) => dispatch(getOfficialThunk(state, index)),
+    getOfficialThunk: (division, officeIndex, officialIndex) =>
+      dispatch(getOfficialThunk(division, officeIndex, officialIndex)),
     getPhoto: (number, state) => dispatch(getPhotoThunk(number, state))
-  };
-};
+  }
+}
 
-export default connect(mapState, mapDispatch)(OfficialContainer);
+export default connect(mapState, mapDispatch)(OfficialContainer)
