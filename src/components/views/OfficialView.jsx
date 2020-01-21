63aa2f7bf2500941fb9161d65206dc58d7c92132
Samson <samson50wu@gmail.com>
2020-01-20 19:20:47 -0500

    Copied addresses from OfficialCard to OfficialView

--- src/components/views/OfficialView.jsx
+++ src/components/views/OfficialView.jsx
@@ -24,8 +24,10 @@ const OfficialView = props => {
     office = props.officialObject.office
     official = props.officialObject.official
   }
+
   console.log(office)
   console.log(official)
+
   // Officials can have anywhere from 0 to 3 channels! We will need to first see if they have any channels at all. If they do, we will then need to iterate through them to generate the elements. Ideally, we should be able to identify the domain of the channel, so that we can link to it directly. eg. https://www.facebook.com/newyorkstateag/ when the type is "Facebook"
   let channels = []
   if (official.channels) {
@@ -83,6 +85,34 @@ const OfficialView = props => {
     }
   }
 
+  // Some officials don't have an address.
+  // The address is an array holding a single object of address lines. Kind of weird.
+  // Some addresses have a line2 and line3 etc in addition to just a line1 (Andrew M. Cuomo). So we'll have to iterate through them somehow, ignoring any that are just blank strings "".
+  let addressLines = []
+  if (official.address) {
+    if (official.address.hasOwnProperty(0)) {
+      let lines = []
+      let line2 = ""
+      for (let key in official.address[0]) {
+        if (["line1", "line2", "line3", "line4"].includes(key)) {
+          // console.log(official.address[0][key])
+          lines.push(<p>{official.address[0][key]}</p>)
+        } else if (key === "city") {
+          // console.log(official.address[0][key])
+          line2 = line2 + official.address[0][key]
+        } else if (key === "state") {
+          // console.log(official.address[0][key])
+          line2 = line2 + ", " + official.address[0][key]
+        } else if (key === "zip") {
+          // console.log(official.address[0][key])
+          line2 = line2 + " " + official.address[0][key]
+        }
+      }
+      addressLines.push(lines)
+      addressLines.push(<p>{line2}</p>)
+    }
+  }
+
   return (
     <div>
       <h2>OfficialView here</h2>
@@ -90,7 +120,8 @@ const OfficialView = props => {
       <p>{divisionId}</p>
       <p>{office.name}</p>
       <p>{official.name}</p>
-      <div>{channels}</div>
+      {addressLines}
+      {channels}
 
       <div className="disqus-container">
         {/* <Disqus.DiscussionEmbed
