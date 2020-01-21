f24b3c632e6b08aa3c951d75be70cb137f647979
Darren Zhang <darrenzhang200027@gmail.com>
2020-01-21 02:17:00 -0500

    rendering messageboards with database data

--- src/components/containers/MessageBoardCollection.jsx
+++ src/components/containers/MessageBoardCollection.jsx
@@ -15,14 +15,21 @@ class MessageBoardCollectionContainer extends Component {
         var res = await axios.get("http://localhost:5000/api/messages/messageboardcollection");
         await this.setState({ officials: res.data});
         console.log(this.state.officials);
+        // console.log("messages array: ", this.state.officials[0].messageboards);
     }
     
     componentDidMount() {
         this.getOfficialsArray(); 
     }
     render(){
+        let officialDisplay = this.state.officials.map(offi => 
+            <div>
+                <h3>Official: {offi.officialName}</h3>
+                {/* <div>{offi.messageboards}</div> */}
+            </div>
+        )
         return <div>
-            
+            {officialDisplay}
         </div>
     }
 }
