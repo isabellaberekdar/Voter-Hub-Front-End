2ca3a1ac39c31278557a81af6133b421d6e98b60
Darren Zhang <darrenzhang200027@gmail.com>
2020-01-21 17:55:05 -0500

    resolved merge conflicts

--- src/components/views/HomeView.jsx
+++ src/components/views/HomeView.jsx
@@ -1,40 +1,5 @@
 import React from "react"
 
-<<<<<<< HEAD
-import { OfficialCard } from "..";
-import { Message } from "..";
-import { MessageBoard } from "..";
-import MessageBoardCollection from "./MessageBoardCollection";
-import MessageBoardCollectionContainer from "../containers/MessageBoardCollection";
-// import MessageBoardCollectionContainer from "..";
-
-// If you need cards or styling, you can uncomment the lines here to import
-// import "./HomeView.css";
-// import { OfficialCard } from "../";
-
-const HomeView = props => {
-
-  //dummy data
-  let msgs = [
-    <Message user="User 1" text="hi yo, this is message 1" />,
-    <Message user="User 2" text="hi yo, this is message 2" />,
-    <Message user="User 3" text="hi yo, this is message 3" />
-  ]
-  let msgboards = [
-    <MessageBoard subject="Message Board" messages={msgs} />,
-    <MessageBoard subject="Message Board" messages={msgs} />,
-    <MessageBoard subject="Message Board" messages={msgs} />
-  ]
-
-
-  return (
-    <div>
-      <h2>HomeView here</h2>
-      <OfficialCard />
-      <MessageBoard subject="Message Board" messages={msgs} />
-      <MessageBoardCollection official="Obama" messageBoardArray={msgboards} />
-      <MessageBoardCollectionContainer/>
-=======
 import { OfficialCard } from ".."
 import "./Home.css"
 
@@ -173,7 +138,6 @@ const HomeView = props => {
           </div>
         </div>
       </div>
->>>>>>> master
     </div>
   )
 }
