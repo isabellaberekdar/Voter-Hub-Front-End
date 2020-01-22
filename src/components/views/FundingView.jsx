163513c3686b1467036b6edfc3138afaf28cfbc1
Samson Wu <13092958+sw385@users.noreply.github.com>
2020-01-21 23:49:35 -0500

    Revert "Get funders"

--- src/components/views/FundingView.jsx
+++ src/components/views/FundingView.jsx
@@ -3,42 +3,21 @@ import PropTypes from "prop-types"
 // import "./Funding.css"
 
 const FundingView = props => {
-  console.log("watercress", props)
   return (
     <div>
-      <h2>Funding for the 2020 cycle:</h2>
+      <h2>Funding</h2>
       <div>
-        {props.funders
-          ? props.funders.map(funder => {
+        {props.articles
+          ? props.articles.map(article => {
               return (
-                <div key={funder["@attributes"].industry_name}>
-                  {/* {console.log(funder["@attributes"].industry_name)}
-                  {console.log(funder["@attributes"].industry_code)}
-                  {console.log(funder["@attributes"].indivs)}
-                  {console.log(funder["@attributes"].pacs)}
-                  {console.log(funder["@attributes"].total)} */}
+                <div key={article.name}>
+                  {/* {console.log()} */}
                   <span>
                     <i>
-                      <b>{funder["@attributes"].industry_name}</b>
+                      <b>{article.provider}</b>
                     </i>
                   </span>
-                  <p>
-                    <b>Individual contributions:</b> $
-                    {funder["@attributes"].indivs}
-                  </p>
-                  <p>
-                    <b>
-                      <a href="https://www.opensecrets.org/pacs/pacfaq.php">
-                        PAC
-                      </a>{" "}
-                      contributions:
-                    </b>{" "}
-                    ${funder["@attributes"].pacs}
-                  </p>
-                  <p>
-                    <b>Total:</b> ${funder["@attributes"].total}
-                  </p>
-                  {/* <a href={article.url}>{article.name}</a> {article.description} */}
+                  <a href={article.url}>{article.name}</a> {article.description}
                 </div>
               )
             })
