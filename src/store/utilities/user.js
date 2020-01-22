35c4f3968424b4d65ded84b2bcff9c558b931efc
Isabella Berekdar <isabellaberekdar@gmail.com>
2020-01-21 23:59:26 -0500

    Added updateUserThunk

--- src/store/utilities/user.js
+++ src/store/utilities/user.js
@@ -1,71 +1,95 @@
-import axios from "axios";
+import axios from "axios"
 
 // ACTION TYPES
-const GET_USER = "GET_USER";
-const REMOVE_USER = "REMOVE_USER";
+const GET_USER = "GET_USER"
+const REMOVE_USER = "REMOVE_USER"
+const UPDATE_USER_INFO = "UPDATE_USER_INFO"
+
 
 // ACTION CREATORS
-const getUser = user => { 
+const getUser = user => {
   return {
     type: GET_USER,
     payload: user
   }
 }
 
-const removeUser = () => { 
-  return { 
-    type: REMOVE_USER 
+const removeUser = () => {
+  return {
+    type: REMOVE_USER
   }
 }
 
+const updateUser = updatedUserInfo => {
+  return {
+    type: UPDATE_USER_INFO,
+    payload: updatedUserInfo
+  }
+}
+
+
 // THUNK CREATORS
+
+// Edit posts
+// not tested yet
+export const updateUserInfoThunk = updatedUserInfo => async dispatch => {
+  try {
+    axios.put('/urltoupdatestuff', updatedUserInfo)
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+
 export const me = () => async dispatch => {
   try {
-    const res = await axios.get("http://localhost:5000/auth/me", { withCredentials: true });
-    dispatch(getUser(res.data || {}));
+    const res = await axios.get("http://localhost:5000/auth/me", { withCredentials: true })
+    dispatch(getUser(res.data || {}))
+  } catch (err) {
+    console.error(err)
   }
-  catch (err) {
-    console.error(err);
-  }
-};
+}
 
 export const auth = (email, password, method) => async dispatch => {
-  let res;
+  let res
   try {
-    res = await axios.post(`http://localhost:5000/auth/${method}`, { email, password }, { withCredentials: true });
-  }
-  catch (authError) {
-    return dispatch(getUser({ error: authError }));
+    res = await axios.post(
+      `http://localhost:5000/auth/${method}`,
+      { email, password },
+      { withCredentials: true }
+    )
+  } catch (authError) {
+    return dispatch(getUser({ error: authError }))
   }
 
   try {
-    dispatch(getUser(res.data));
+    dispatch(getUser(res.data))
+  } catch (dispatchOrHistoryErr) {
+    console.error(dispatchOrHistoryErr)
   }
-  catch (dispatchOrHistoryErr) {
-    console.error(dispatchOrHistoryErr);
-  }
-};
+}
 
 export const logout = () => async dispatch => {
   try {
-    await axios.delete("http://localhost:5000/auth/logout", { withCredentials: true });
-    dispatch(removeUser());
+    await axios.delete("http://localhost:5000/auth/logout", { withCredentials: true })
+    dispatch(removeUser())
+  } catch (err) {
+    console.error(err)
   }
-  catch (err) {
-    console.error(err);
-  }
-};
+}
 
 // REDUCER
 const reducer = (state = {}, action) => {
   switch (action.type) {
     case GET_USER:
-      return action.payload;
+      return action.payload
     case REMOVE_USER:
-      return {};
+      return {}
+    case UPDATE_USER_INFO:
+      return action.payload
     default:
-      return state;
+      return state
   }
 }
 
-export default reducer;
+export default reducer
