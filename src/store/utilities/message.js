b483e03f8711fe65ec1ef36dfdaf4033fddf7185
Darren Zhang <darrenzhang200027@gmail.com>
2020-01-23 11:58:48 -0500

    synced post message database functionality with store

--- src/store/utilities/message.js
+++ src/store/utilities/message.js
@@ -2,7 +2,6 @@ import axios from "axios"
 
 // messageboard = a collection of threads
 
-
 // ACTION TYPES
 const GET_MESSAGEBOARD = "GET_MESSAGEBOARD"
 const POST_MESSAGE = "POST_MESSAGE"
@@ -11,123 +10,93 @@ const GET_THREAD = "GET_THREAD"
 // ACTION CREATORS
 
 const getMessageBoard = messageBoard => {
-    return {
-        type: GET_MESSAGEBOARD,
-        payload: messageBoard
-    }
+  return {
+    type: GET_MESSAGEBOARD,
+    payload: messageBoard
+  }
 }
 
 const postMessage = message => {
-    return {
-        type: POST_MESSAGE,
-        payload: message
-    }
+  return {
+    type: POST_MESSAGE,
+    payload: message
+  }
 }
 
 const getThread = messages => {
-    return {
-        type: GET_THREAD,
-        payload: messages
-    }
+  return {
+    type: GET_THREAD,
+    payload: messages
+  }
 }
 
-//return all the threads 
+//return all the threadsnpm
 export const getMessageBoardThunk = messageBoardId => async dispatch => {
-    // console.log(address);
-    try {
-        console.log("blueberry")
-      // Query the api for the officials associated with the given address
-      const { data } = await axios.get(
-        `http://localhost:5000/api/messages/messageboard/${messageBoardId}`
-      )
-      console.log("cantaloupe", data)
+  // console.log(address);
+  try {
+    console.log("blueberry")
+    // Query the api for the officials associated with the given address
+    const { data } = await axios.get(
+      `http://localhost:5000/api/messages/messageboard/${messageBoardId}`
+    )
+    console.log("cantaloupe", data)
     dispatch(getMessageBoard(data))
-    } catch (error) {
-      console.log("Error in getOfficialsThunk:", error)
-    }
+  } catch (error) {
+    console.log("Error in getOfficialsThunk:", error)
   }
-//return all the threads 
+}
+
+//return all the threads
 export const getThreadThunk = threadId => async dispatch => {
-    // console.log(address);
-    try {
-        console.log("penguin berry")
-      // Query the api for the officials associated with the given address
-      const { data } = await axios.get(
-        `http://localhost:5000/api/messages/messageboard/thread/${threadId}`
-      )
-      console.log("cantaloupe berry", data)
-    dispatch(getThread(data))
-    } catch (error) {
-      console.log("Error in getThreadThunk:", error)
-    }
+  // console.log(address);
+  try {
+    console.log("penguin berry")
+    // Query the api for the officials associated with the given address
+    const { data } = await axios.get(
+      `http://localhost:5000/api/messages/messageboard/thread/${threadId}`
+    )
+    console.log("cantaloupe berry", data.messages)
+    dispatch(getThread(data.messages))
+  } catch (error) {
+    console.log("Error in getThreadThunk:", error)
   }
+}
 
-
-
-
-// // THUNK CREATORS
-// export const getOfficialsThunk = searchbarValue => async dispatch => {
-//   // console.log(address);
-//   try {
-//     // Query the api for the officials associated with the given address
-//     const { data } = await axios.get(
-//       `https://www.googleapis.com/civicinfo/v2/representatives?address=${searchbarValue}&key=AIzaSyCzgqBJLDzmJQo5Cj7PVBKr7DS8fdH-c8M`
-//     )
-//     // console.log("cantaloupe", data)
-//     dispatch(getOfficials(data))
-//   } catch (error) {
-//     console.log("Error in getOfficialsThunk:", error)
-//   }
-// }
+export const postMessageThunk = message => async dispatch => {
+  try {
+    let newMessage = await axios.post(
+      "http://localhost:5000/api/messages",
+      message
+    )
+    console.log("pikachu", newMessage.datas)
+    dispatch(postMessage(newMessage.data))
+  } catch (error) {
+    console.log("Error in postMessageThunk", error)
+  }
+}
 
 const initialState = {}
 
 // REDUCER
 const officialReducer = (state = initialState, action) => {
   switch (action.type) {
-        case GET_MESSAGEBOARD:
-            return {
-                ...state, 
-                threads: action.payload
-            }
-        case GET_THREAD:
-            return {
-                ...state,
-                messages: action.payload
-            }
-        
-    // case GET_OFFICIALS:
-    //   // create a new object, copy over everything from state, then add the new officials data that was fetched
-    //   return {
-    //     ...state,
-    //     officials: action.payload
-    //   }
-    // case GET_OFFICIAL:
-    //   // create a new object, copy over everything from state, then add the new officials data that was fetched
-    //   return {
-    //     ...state,
-    //     official: action.payload
-    //   }
-    // case GET_PHOTO:
-    //   return {
-    //     ...state,
-    //     photo: action.payload
-    //   }
-    // case GET_ARTICLES:
-    //   return {
-    //     ...state,
-    //     articles: action.payload
-    //   }
-    // case STORE_NAME:
-    //   return {
-    //     ...state,
-    //     nameObj: action.payload
-    //   }
-    // case GET_CID:
-    //   return {
-    //     ...state,
-    //     cid: action.payload
-    //   }
+    case GET_MESSAGEBOARD:
+      return {
+        ...state,
+        threads: action.payload
+      }
+    case GET_THREAD:
+      return {
+        ...state,
+        messages: action.payload
+      }
+    case POST_MESSAGE:
+      console.log("chicken", state.messages)
+      return {
+        ...state,
+        messages: [...state.messages, action.payload]
+      }
+
     default:
       return state
   }
